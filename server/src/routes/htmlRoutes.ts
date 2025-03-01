ea7c39e5d2f5f8a83a471d7d987ddd222716aa85
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-01 12:46:14 -0600

    server setup

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -2,7 +2,10 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
 const __filename = fileURLToPath(import.meta.url);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const __dirname = path.dirname(__filename);
+
 const router = Router();
 
 // TODO: Define route to serve index.html
