61ede56dc08b540e70f8496d2a2c9f2613f19393
Alex Menendez <alexis.menendez@austincc.edu>
2025-03-03 16:07:47 -0600

    Update htmlRoutes.ts
    debugging

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -3,16 +3,16 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
 
-//Commented out to debug, will remove if debug works
+
 // define the router and resolve __dirname & __filename
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
 router.get('/', (_req, res) => {
   try {
-    const filePath = path.join(__dirname, '../../public/index.html');
+    const filePath = path.join(__dirname, '../../client/index.html');
     
     // Log file path for debugging
     console.log(`📂 Attempting to serve index.html from: ${filePath}`);
