8e3e5c99bd100ce2873b9c0ebd100d59197c8027
Alex Menendez <alexis.menendez@austincc.edu>
2025-03-03 14:55:35 -0600

    Update htmlRoutes.ts
    commented a section out to test debug

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -3,9 +3,10 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
 
+//Commented out to debug, will remove if debug works
 // define the router and resolve __dirname & __filename
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
