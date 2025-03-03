786f3354decd89aaeabe9821038d00a431887817
Alex Menendez <alexis.menendez@austincc.edu>
2025-03-03 11:42:01 -0600

    Update htmlRoutes.ts
    debugging route to serve index.html

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -3,14 +3,13 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
 
-// COMPLETED: Define the router and resolve __dirname & __filename
+// define the router and resolve __dirname & __filename
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
 // COMPLETED: Define route to serve index.html
-router.get('/', (req, res) => { // define the route 
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html')); // send the index.html file
-  });
-  
-  export default router; // export the router
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+export default router;
