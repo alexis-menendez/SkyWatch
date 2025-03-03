8f846210d97ea31cc338c38d30c63760d178eff8
Alex Menendez <alexis.menendez@austincc.edu>
2025-03-03 11:42:28 -0600

    Update htmlRoutes.ts

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// COMPLETED: Define route to serve index.html
+// TODO: Define route to serve index.html
 router.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
