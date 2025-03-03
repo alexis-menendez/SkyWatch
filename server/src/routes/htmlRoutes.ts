4b701cbf9ba66ee1c435d9e147b9e7c040fdec32
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-02 20:11:01 -0600

    htmlRoutes.ts
    added comments to make it easier for me to troubleshoot later

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,13 +1,16 @@
+// import modules
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
+
+// Define the router and resolve __dirname & __filename
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+router.get('/', (req, res) => { // define the route 
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html')); // send the index.html file
   });
   
-  export default router;
+  export default router; // export the router
