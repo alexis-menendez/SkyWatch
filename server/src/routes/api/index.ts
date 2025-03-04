d464cad5e193c66f4bc7383bd2fa99421ed6cd8a
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-03 19:09:23 -0600

    starter file issue
    I realized that somehow my starter files were not the same as others. I've moved the correct start files into my repo so I can start again

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 
-import weatherRoutes from './weatherRoutes.js'; // added this, commented out others to test debug
-// import apiRoutes from './api/index.js';
-// import htmlRoutes from './htmlRoutes.js';
+import weatherRoutes from './weatherRoutes.js';
 
-router.use('/weather', weatherRoutes); // added this, commented out others to test debug
-// router.use('/api', apiRoutes);
-// router.use('/', htmlRoutes);
+router.use('/weather', weatherRoutes);
 
 export default router;
